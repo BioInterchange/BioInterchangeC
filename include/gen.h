408f9e1a50c0c53738cac89bd0d654f0a754be9b
Joachim Baran <joachim.baran@gmail.com>
2015-03-31 20:55:34 -0400

    Added files for handling generic genomics file formats.

---
+++ include/gen.h
@@ -0,0 +1,13 @@
+//
+//  gen.h
+//  biointerchange
+//
+//  Created by Joachim Baran on 2015-03-31.
+//
+//
+
+#ifndef biointerchange_gen_h
+#define biointerchange_gen_h
+
+
+#endif
---
+++ src/biointerchange/gen.c
@@ -0,0 +1,9 @@
+//
+//  gen.c
+//  biointerchange
+//
+//  Created by Joachim Baran on 2015-03-31.
+//
+//
+
+#include "gen.h"
