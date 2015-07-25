96acab47b5516be1e890e08a06e586808b906ea0
Joachim Baran <joachim.baran@gmail.com>
2015-07-24 22:55:48 -0400

    Added build script for Homebrew.

---
+++ build-brew.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+
+set -e
+
+license_version=1
+version="`./biointerchange -v`"
+
+mkdir -p brew/biointerchange
+
+cp biointerchange brew/biointerchange
+cp biointerchange-l$license_version.txt brew/biointerchange/license.txt
+
+cd brew
+
+if [[ -f "biointerchange-$version.tar.gz" ]] ; then
+    rm -f "biointerchange-$version.tar.gz"
+fi
+
+tar cfz "biointerchange-$version.tar.gz" biointerchange
+
+cd ..
+
+shasum "brew/biointerchange-$version.tar.gz"
+
