c23ba199df4368c1ec0e1c7dfae0246f57efbbba
Joachim Baran <joachim.baran@gmail.com>
2021-03-13 11:54:29 -0500

    Updated source-file headers (copyright notice).

--- src/biointerchange/so.c
+++ src/biointerchange/so.c
@@ -1,14 +1,10 @@
 /*
- * Copyright (c) 2015 CODAMONO, Ontario, Canada
+ * Copyright (c) 2021 Joachim Baran
+ * Copyright (c) 2015-2020 CODAMONO, Ontario, Canada
  *
  * This Source Code Form is subject to the terms of the accompanying
- * LICENSE.txt file, or, available via the following URLs:
+ * LICENSE.txt file.
  *
- *   As text: http://www.codamono.com/license/biointerchange-l1.txt
- *
- *   As PDF:  http://www.codamono.com/license/biointerchange-l1.pdf
- *
- *   As DOCX: http://www.codamono.com/license/biointerchange-l1.docx
  */
 
 #include "so.h"
