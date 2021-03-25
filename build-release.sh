d00e9d94633799f738f6ece12057c0fb628a91b1
Joachim Baran <joachim.baran@gmail.com>
2021-03-25 18:01:24 -0400

    Removed all external projects except for LibDocument.

--- build-release.sh
+++ build-release.sh
@@ -4,11 +4,6 @@ set -e
 
 cmake -DCMAKE_BUILD_TYPE=Release -G 'Unix Makefiles'
 
-if [[ "`uname`" == "Darwin" ]] ; then
-    make openssl-lib
-fi
-
-make curl-lib
 make libdocument-lib
 
 make biointerchange
